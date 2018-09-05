35e21f16933e6720d3d2805e6688cc512eb3328a
unknown <valentin.blay@gmail.com>
2018-09-05 15:47:55 +0200

    add modif algo.js

---
+++ js/algo.js
@@ -0,0 +1,282 @@
+eau_raccorde = true;
+cle_admin = false;
+console.log(typeof(cle_admin))//boolean
+
+var stock_cafe = Number(10);
+var stock_chocolat = 10;
+var stock_the = 10;
+var stock_lait = 10;
+var stock_cappuccino = 10;
+var stock_sucre = 50;
+var stock_gobelet = 50;
+var stock_touillette = 50;
+var nom = new String("javascript");
+
+
+if (eau_raccorde === true){
+        
+    if (cle_admin){
+
+        print_screen("Selectionner une boisson");
+
+        Choice_Expresso{
+            if (stock_cafe = !0, stock_gobelet = !0, stock_touillette = !0){
+            print_screen("Inserer 0.70cts")
+            if (money >= 0.70){
+                if (money == 0.70){
+                    print_screen("Selectioner le sucre");
+                    user_select_sucre;
+                    sucre - user_select_sucre;
+                    start_warming_water();
+                    delay;
+                    pick_Expresso()
+                    stock_cafe -1;
+                    pick_gobelet()
+                    stock_gobelet -1;
+                    pick_touillette()
+                    stock_touillette -1;
+                    delay;
+                }else{
+                    rendre_monnaie();
+                    start_warming_water();
+                    delay;
+                    pick_Expresso()
+                    stock_cafe -1;
+                    pick_gobelet()
+                    stock_gobelet -1;
+                    pick_touillette()
+                    stock_touillette -1;
+                    delay;
+                }
+            }else{
+                print_screen("Ajouter de la monnaie");
+            }
+        }else{
+            print_screen("stock indisponible, selectionner une autre boisson");
+        }
+    }
+
+        Choice_Long{
+            if (stock_cafe = !0, stock_gobelet = !0, stock_touillette = !0){
+                print_screen("Inserer 0.70cts")
+                if (money >= 0.70){
+                    if (money == 0.70){
+                        print_screen("Selectioner le sucre");
+                        user_select_sucre;
+                        sucre - user_select_sucre;
+                        start_warming_water();
+                        delay;
+                        pick_long()
+                        stock_cafe -1;
+                        pick_gobelet()
+                        stock_gobelet -1;
+                        pick_touillette()
+                        stock_touillette -1;
+                        delay;
+                    }else{
+                        rendre_monnaie();
+                        print_screen("Selectioner le sucre");
+                        user_select_sucre;
+                        sucre - user_select_sucre;
+                        start_warming_water();
+                        delay;
+                        pick_long()
+                        stock_cafe -1;
+                        pick_gobelet()
+                        stock_gobelet -1;
+                        pick_touillette()
+                        stock_touillette -1;
+                        delay;
+                    }
+                }else{
+                    print_screen("Ajouter de la monnaie");
+                }
+            }else{
+                print_screen("stock indisponible, selectionner une autre boisson");
+            }
+        }
+
+        Choice_ChocolatChaud{
+            if (stock_chocolat = !0, stock_gobelet = !0, stock_touillette = !0){
+                print_screen("Inserer 0.70cts")
+                if (money >= 0.70){
+                    if (money == 0.70){
+                        print_screen("Selectioner le sucre");
+                        user_select_sucre;
+                        sucre - user_select_sucre;
+                        start_warming_water();
+                        delay;
+                        pick_chocolat_chaud()
+                        stock_chocolat -1;
+                        pick_gobelet()
+                        stock_gobelet -1;
+                        pick_touillette()
+                        stock_touillette -1;
+                        delay;
+                    }else{
+                        rendre_monnaie();
+                        print_screen("Selectioner le sucre");
+                        user_select_sucre;
+                        sucre - user_select_sucre;
+                        start_warming_water();
+                        delay;
+                        pick_chocolat_chaud()
+                        stock_chocolat -1;
+                        pick_gobelet()
+                        stock_gobelet -1;
+                        pick_touillette()
+                        stock_touillette -1;
+                        delay;
+                    }
+                }else{
+                    print_screen("Ajouter de la monnaie");
+                }
+            }else{
+                print_screen("stock indisponible, selectionner une autre boisson");
+            }
+        }
+
+        Choice_The_Vert{
+            if (stock_the = !0, stock_gobelet = !0, stock_touillette = !0){
+                print_screen("Inserer 0.60cts")
+                if (money >= 0.60){
+                    if (money == 0.60){
+                        print_screen("Selectioner le sucre");
+                        user_select_sucre;
+                        sucre - user_select_sucre;
+                        start_warming_water();
+                        delay;
+                        pick_the_vert()
+                        stock_the -1;
+                        pick_gobelet()
+                        stock_gobelet -1;
+                        pick_touillette()
+                        stock_touillette -1;
+                        delay;
+                    }else{
+                        rendre_monnaie();
+                        start_warming_water();
+                        delay;
+                        pick_the_vert()
+                        stock_the -1;
+                        pick_gobelet()
+                        stock_gobelet -1;
+                        pick_touillette()
+                        stock_touillette -1;
+                        delay;
+                    }
+                }else{
+                    print_screen("Ajouter de la monnaie");
+                }
+            }else{
+                print_screen("stock indisponible, selectionner une autre boisson");
+            }
+        }
+
+        Choice_Cappuccino{
+            if (stock_cappuccino = !0, stock_gobelet = !0, stock_touillette = !0){
+                print_screen("Inserer 0.70cts")
+                if (money >= 0.70){
+                    if (money == 0.70){
+                        print_screen("Selectioner le sucre");
+                        user_select_sucre;
+                        sucre - user_select_sucre;
+                        start_warming_water();
+                        delay;
+                        pick_cappuccino()
+                        stock_cappuccino -1;
+                        pick_gobelet()
+                        stock_gobelet -1;
+                        pick_touillette()
+                        stock_touillette -1;
+                        delay;
+                    }else{
+                        rendre_monnaie();
+                        start_warming_water();
+                        delay;
+                        pick_cappuccino()
+                        stock_cappuccino -1;
+                        pick_gobelet()
+                        stock_gobelet -1;
+                        pick_touillette()
+                        stock_touillette -1;
+                        delay;
+                    }
+                }else{
+                    print_screen("Ajouter de la monnaie");
+                }
+            }else{
+                print_screen("stock indisponible, selectionner une autre boisson");
+            }
+        }
+
+        Choice_Lait{
+            if (stock_lait = !0, stock_gobelet = !0, stock_touillette = !0){
+                print_screen("Inserer 0.70cts")
+                if (money >= 0.50){
+                    if (money == 0.50){
+                        print_screen("Selectioner le sucre");
+                        user_select_sucre;
+                        sucre - user_select_sucre;
+                        start_warming_water();
+                        delay;
+                        pick_lait();
+                        stock_lait -1;
+                        pick_gobelet();
+                        stock_gobelet -1;
+                        pick_touillette();
+                        stock_touillette -1;
+                        delay;
+                    }else{
+                        rendre_monnaie();
+                        start_warming_water();
+                        delay;
+                        pick_lait();
+                        stock_lait -1;
+                        pick_gobelet();
+                        stock_gobelet -1;
+                        pick_touillette();
+                        stock_touillette -1;
+                        delay;
+                    }
+                }else{
+                    print_screen("Ajouter de la monnaie");
+                }
+            }else{
+                print_screen("stock indisponible, selectionner une autre boisson");
+            }
+        }
+            
+    }else{
+
+        displayStock_gobelet;
+        displayStock_touillette;
+
+        listener button_1;
+        displayStock_cafe;
+
+        listener button_2;
+        displayStock_cafe;
+
+        listener button_3;
+        displayStock_chocolat;
+
+        listener button_4;
+        displayStock_the;
+
+        listener button_5;
+        displayStock_cappuccino;
+
+        listener button_6;
+        displayStock_lait;
+        
+        listener button_less_sugar;
+        displayStock_sucre;
+
+        listener button_more_sugar;
+        displayStock_sucre;
+
+    }
+}else{ 
+    console.log("Non raccordé à l'eau, contacter un technicien");
+}
